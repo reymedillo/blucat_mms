6cf6b526d81fc9e5e45d8cf9fa2f887152517cd8
mark <mark@ipp.ph>
2016-12-27 16:55:28 +0800

    update print

--- resources/views/wifi/print.blade.php
+++ resources/views/wifi/print.blade.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-<head></head>
-    <body onload="window.print();window.close();">
+<head>
+	<script type="text/javascript">
+		window.onload = function () {
+		  window.print();
+		  setTimeout(function(){window.close();}, 1);
+		}
+	</script>
+</head>
+    <body>
 	    {{$date}} ID:{{$mid}}<h1>{{$pw}}</h1>
 	</body>
 </html>
--- resources/views/wifi/printGuest.blade.php
+++ resources/views/wifi/printGuest.blade.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-<head></head>
-    <body onload="window.print();window.close();">
+<head>
+	<script type="text/javascript">
+		window.onload = function () {
+		  window.print();
+		  setTimeout(function(){window.close();}, 1);
+		}
+	</script>
+</head>
+    <body>
 	    {{$date}} ID: GUEST<h1>{{$pw}}</h1>
 <font size="1">Apply for weekly or monthly membership.<br />
 Weekly only P{{$weekly_fee}}.<br />
